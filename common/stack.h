902dba6db77e206090befa8f1c930c8123c5b2d2
mb467 <buninm03@gmail.com>
2025-03-17 10:07:49 +0300

    Update stack.h

--- common/stack.h
+++ common/stack.h
@@ -3,43 +3,58 @@
 
 using namespace std;
 
+/// @brief класс Stack реализует стек с использованием связного списка
+///@tparam <т> тип данных, который будет храниться в стеке
+
 template <typename T>
 class Stack
 {
 private:
+/// @brief внутренний класс Node представляет узел связного списка
     class Node
     {
     public:
-        T data;
-        Node* next;
-
+        T data; 
+        Node* next; 
+ /// @brief конструктор узла
+ /// @param data Данные, которые будут храниться в узле
+ /// @param next Указатель на следующий узел (по умолчанию nullptr)
         Node(T data, Node* next = nullptr)
         {
             this->data = data;
             this->next = next;
         }
-    }
+    };
 
-    Node* head;
-    int counter;
+    Node* head; 
+    int counter; 
 
 public:
-    Stack()
-    {
-        head = nullptr;
-        counter = 0;
-    }
+/// @brief Конструктор стека
 
+    Stack() : head(nullptr), counter(0) {}
+
+/// @brief Деструктор стека
+/// Освобождает память, занятую элементами стека
+/// 
     ~Stack()
     {
         clear();
     }
 
+///@brief Добавляет элемент на вершину стека
+///@param value Значение, которое будет добавлено в стек
+/// 
     void push(T value)
     {
         head = new Node(value, head);
         counter++;
     }
+
+
+///@brief удаляет и возвращает элемент с вершины стека
+///@return T значение, которое было на вершине стека
+///@throws out_of_range Если стек пуст
 
     T pop()
     {
@@ -55,6 +70,10 @@ public:
         return value;
     }
 
+     ///@brief Возвращает значение элемента на вершине стека без его удаления.
+     ///@return T Значение, которое находится на вершине стека.
+     ///@throws out_of_range Если стек пуст.
+
     T peek() const
     {
         if (head == nullptr)
@@ -64,10 +83,15 @@ public:
         return head->data;
     }
 
+     ///@brief Возвращает количество элементов в стеке.
+     ///@return int Количество элементов в стеке.
+     /// 
     int count() const
     {
         return counter;
     }
+
+     /// @brief Очищает стек, удаляя все элементы.
 
     void clear()
     {
